813764f0a74ee9a3c4315fd362ff8dbecc3a7131
Faniry1015 <frandriaharimino@yahoo.com>
2025-03-08 19:08:23 +0300

    update

--- tp_mld.sql
+++ tp_mld.sql
@@ -1,7 +1,11 @@
 -- SQLite
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS categories_recipes;
+
 DROP TABLE IF EXISTS recipes;
+
+DROP TABLE IF EXISTS categories;
 
 DROP TABLE IF EXISTS users;
 
@@ -27,12 +31,46 @@ CREATE TABLE
 CREATE TABLE
     categories (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        title VARCHAR(150) NOT NULL,
+        title VARCHAR(150) NOT NULL
     );
 
-CREATE TABLE categories_recipes (
-    recipe_id INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
-    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE NULL
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE NULL,
-);
+CREATE TABLE
+    categories_recipes (
+        recipe_id INTEGER NOT NULL,
+        category_id INTEGER NOT NULL,
+        FOREIGN KEY (recipe_id) REFERENCES recipes (id) ON DELETE CASCADE,
+        FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE,
+        PRIMARY KEY (recipe_id, category_id),
+        UNIQUE (recipe_id, category_id)
+    );
+
+INSERT INTO
+    users (username, email)
+VALUES
+    ('John Doe', 'john@doe.fr');
+
+INSERT INTO
+    categories (title)
+VALUES
+    ('entrée'),
+    ('plat'),
+    ('dessert');
+
+INSERT INTO
+    recipes (title, slug, preparation, user_id)
+VALUES
+    ('Poulet', 'poulet', 'Cuisiner le poulet', 1),
+    ('Steak', 'steak', 'Cuisiner le steak', 1),
+    ('Poulet et Steak', 'poulet-et-steak', 'Cuisiner le poulet et le steak', 1),
+    ('Poulet et Steak et fromage', 'poulet-et-steak-et-fromage', 'Cuisiner le poulet et le steak et le fromage', 1);
+
+INSERT INTO
+    categories_recipes (recipe_id, category_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 1),
+    (3, 2),
+    (4, 1),
+    (4, 2),
+    (4, 3);
